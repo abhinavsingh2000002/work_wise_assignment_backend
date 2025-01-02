b30a8dcda0b647f80d4a13710797508a8c1772cc
Abhinav Singh <abhinavsingh2000002@gmail.com>
2025-01-03 00:02:39 +0530

    8

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,7 +5,7 @@ import upload from "../middlewares/multer.middleware.js";
 const router=Router();
 
 router.route("/register").post(upload.single('file'),registerUser);
-router.route("/login").post(upload.single('file'), loginUser);
+// router.route("/login").post(upload.single('file'), loginUser);
 router.route("/logout").post(upload.single('file'), logoutUser);
 
 
